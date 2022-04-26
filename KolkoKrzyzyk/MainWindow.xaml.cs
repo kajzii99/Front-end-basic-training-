a6562c595766be409058b607a0d773b1e527d9d8
Matthew <mateusz.kajzer2807pl@gmail.com>
2022-04-26 10:49:12 +0200

    Add files via upload
    Basic Tic Tac Toe application using C# and Win Forms

---
+++ KolkoKrzyzyk/MainWindow.xaml.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KolkoKrzyzyk
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public bool TuraGraczaPierwszego { get; set; } //pierwszy gracz zaczyna kolkiem
+
+        public int Licznik { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            NowaGra();
+        }
+
+        public void NowaGra() //pierwszy gracz zawsze jest kolkiem
+        {
+            TuraGraczaPierwszego = false;
+            Licznik = 0;
+
+            Button_0_0.Content = string.Empty;
+            Button_1_0.Content = string.Empty;
+            Button_2_0.Content = string.Empty;
+            Button_0_1.Content = string.Empty;
+            Button_1_1.Content = string.Empty;
+            Button_2_1.Content = string.Empty;
+            Button_0_2.Content = string.Empty;
+            Button_1_2.Content = string.Empty;
+            Button_2_2.Content = string.Empty;
+            
+
+            Button_0_0.Background = Brushes.White;
+            Button_1_0.Background = Brushes.White;
+            Button_2_0.Background = Brushes.White;
+            Button_0_1.Background = Brushes.White;
+            Button_1_1.Background = Brushes.White;
+            Button_2_1.Background = Brushes.White;
+            Button_0_2.Background = Brushes.White;
+            Button_1_2.Background = Brushes.White;
+            Button_2_2.Background = Brushes.White;
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e) //funkcja przy kliknięciu
+        {
+            //zamiana tur graczy 
+            if (TuraGraczaPierwszego) TuraGraczaPierwszego = false;
+            else TuraGraczaPierwszego = true;
+
+            //licznik zapelnionych pól; 10 = nowa gra
+            Licznik++;
+
+            if (Licznik > 9)
+            {
+                NowaGra();
+                return;
+            }
+
+
+            var button = sender as Button; //kastowanie sendera na buttona 
+
+            if (TuraGraczaPierwszego)
+            {
+                button.Content = "O";
+            }
+
+            else
+            {
+                button.Content = "X";
+            }
+               
+
+            if (SprawdzCzyKtosWygral())
+            {
+                Licznik = 9;
+            }
+        }
+
+        private bool SprawdzCzyKtosWygral()
+        {
+            if (Button_0_0.Content.ToString() != string.Empty && Button_0_0.Content == Button_0_1.Content && Button_0_1.Content == Button_0_2.Content)
+            {
+                Button_0_0.Background = Brushes.Green;
+                Button_0_1.Background = Brushes.Green;
+                Button_0_2.Background = Brushes.Green;
+                return true;
+            }
+
+            if (Button_1_0.Content.ToString() != string.Empty && Button_1_0.Content == Button_1_1.Content && Button_1_1.Content == Button_1_2.Content)
+            {
+                Button_1_0.Background = Brushes.Green;
+                Button_1_1.Background = Brushes.Green;
+                Button_1_2.Background = Brushes.Green;
+                return true;
+            }
+
+            if (Button_2_0.Content.ToString() != string.Empty && Button_2_0.Content == Button_2_1.Content && Button_2_1.Content == Button_2_2.Content)
+            {
+                Button_2_0.Background = Brushes.Green;
+                Button_2_1.Background = Brushes.Green;
+                Button_2_2.Background = Brushes.Green;
+                return true;
+            }
+
+            if (Button_0_0.Content.ToString() != string.Empty && Button_0_0.Content == Button_1_0.Content && Button_1_0.Content == Button_2_0.Content)
+            {
+                Button_0_0.Background = Brushes.Green;
+                Button_1_0.Background = Brushes.Green;
+                Button_2_0.Background = Brushes.Green;
+                return true;
+            }
+
+            if (Button_0_1.Content.ToString() != string.Empty && Button_0_1.Content == Button_1_1.Content && Button_1_1.Content == Button_2_1.Content)
+            {
+                Button_0_1.Background = Brushes.Green;
+                Button_1_1.Background = Brushes.Green;
+                Button_2_1.Background = Brushes.Green;
+                return true;
+            }
+
+            if (Button_0_2.Content.ToString() != string.Empty && Button_0_2.Content == Button_1_2.Content && Button_1_2.Content == Button_2_2.Content)
+            {
+                Button_0_2.Background = Brushes.Green;
+                Button_1_2.Background = Brushes.Green;
+                Button_2_2.Background = Brushes.Green;
+                return true;
+            }
+
+            if (Button_0_0.Content.ToString() != string.Empty && Button_0_0.Content == Button_1_1.Content && Button_1_1.Content == Button_2_2.Content)
+            {
+                Button_0_0.Background = Brushes.Green;
+                Button_1_1.Background = Brushes.Green;
+                Button_2_2.Background = Brushes.Green;
+                return true;
+            }
+
+            if (Button_0_2.Content.ToString() != string.Empty && Button_0_2.Content == Button_1_1.Content && Button_1_1.Content == Button_2_0.Content)
+            {
+                Button_0_2.Background = Brushes.Green;
+                Button_1_1.Background = Brushes.Green;
+                Button_2_0.Background = Brushes.Green;
+                return true;
+            }
+
+            return false;
+        }
+    }
+} 
+
